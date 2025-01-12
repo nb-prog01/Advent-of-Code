fd8fddd8c9b3381a3ed1c6ec793a2f92858bb964
nb-prog01 <nirajbhadoria007@gmail.com>
2025-01-13 02:25:30 +0530

    XMAS

--- 2024/Day4-XMAS/xmas.py
+++ 2024/Day4-XMAS/xmas.py
@@ -1,68 +1,88 @@
 import numpy as np
 import time
-import re
+import regex as re
 
 start=time.time()
 
 # #PART 1-SOLUTION
-# data_array=np.loadtxt ('xmas.txt',dtype='object')
-# data_array=np.array(data_array)
-# print(type(data_array))
+data_array=np.loadtxt ('xmas.txt',dtype='object')
+data_array=np.array(data_array)
+print(type(data_array))
+
+key="XMAS"
+HCOUNT=0
+VCOUNT=0
+LDCOUNT=0
+RDCOUNT=0
+
+vertical=np.empty(shape=(140,140),dtype='object')
+horizontal=np.empty(shape=(140,140),dtype='object')
 
 f=open("xmas.txt")
 blob=f.read()
-f.close()
-
-key="XMAS"
-
 blob=blob.replace(""," ")
 
 np_array=np.genfromtxt(blob.splitlines(),dtype='U')
-print(len(np_array[0]))
-
-#PART 1-SOLUTION
-
-output1=[]
-def dotAttachedNumbers(num,lineNo,start,end):
-    
-    kagi=np.array('.')
-    lenOfNum=len(str(num))
-    dictOfIndices={
-                    'left':[lineNo,(start-1)],
-                    'left_diagonal':[(lineNo-1),(start-1)],
-                    'left_neg_diagonal':[(lineNo+1),(start-1)],
-                    'right':[(lineNo),(end+1)],
-                    'right_diagonal':[(lineNo-1),(end+1)],
-                    'right_neg_diagonal':[(lineNo+1),(end+1)]
-                  }
-    for i in range (lenOfNum):
-        key1= 'centre_up_'+ str(i)
-        key2= 'centre_down_'+str(i)
-        dictOfIndices[key1]=[(lineNo-1),(start+i)]
-        dictOfIndices[key2]=[(lineNo+1),(start+i)]
-  
-    for key, value in dictOfIndices.items():
-           try:
-                  if(value[0]==-1 or value[1]==-1):
-                        pass
-                  elif((np.char.not_equal(data_array[value[0]][value[1]],kagi))and not(np.char.isdigit(data_array[value[0]][value[1]]))):
-                        output1.append(int(num)) 
-                        data_array[lineNo]=data_array[lineNo][:start]+('.'*lenOfNum)+data_array[lineNo][(end+1):]
-                        break
-                  else:
-                        data_array[lineNo]=data_array[lineNo][:start]+('.'*lenOfNum)+data_array[lineNo][(end+1):]
-           except IndexError:
-                  pass
-           
-
+# print(np_array)
 
 for i in range(len(data_array)):
-    numbers=re.findall(r'\d+',data_array[i])
-    for j in range(len(numbers)):
-        
-            m=next(re.finditer(numbers[j],data_array[i]))    
-            st=(m.start())
-            ed=(m.end()-1)
-            dotAttachedNumbers(numbers[j],i,st,ed)
+      for j in range(len(data_array)):
+            vertical[j][i]=str(data_array[i][j])
 
-print("PART 1 SOLUTION: ",sum(output1))
+horizontal=np.rot90(np.fliplr(vertical))
+
+for i in range(len(horizontal)):
+      x_str=''.join(str(a) for a in horizontal[i])
+      # print(x_str)
+      pattern=r"XMAS|SAMX"
+      matches=re.findall(pattern,x_str,overlapped=True)
+      # print(matches)
+      HCOUNT+=len(matches)
+
+print("Horizontal count:",HCOUNT)
+
+for i in range(len(vertical)):
+      x_str=''.join(str(a) for a in vertical[i])
+      # print(x_str)
+      pattern=r"XMAS|SAMX"
+      matches=re.findall(pattern,x_str,overlapped=True)
+      # print(matches)
+      VCOUNT+=len(matches)
+
+print("Vertical count:",VCOUNT)
+
+#left to right diagonal
+for i in range(-np_array.shape[0]+1,np_array.shape[0]):
+      olo=np_array.diagonal(offset=i)
+      x_str=''.join(str(a) for a in olo)
+      # print(x_str)
+      pattern=r"XMAS|SAMX"
+      matches=re.findall(pattern,x_str,overlapped=True)
+      # print(matches)
+      LDCOUNT+=len(matches)
+
+print("LD count:",LDCOUNT)
+
+#right to left diagonal
+
+diagonalmat=np.rot90(horizontal)
+
+for i in range(-horizontal.shape[0]+1,horizontal.shape[0]):
+      polo=diagonalmat.diagonal(offset=i)
+      x_str=''.join(str(a) for a in polo)
+      # print(x_str)
+      pattern=r"XMAS|SAMX"
+      matches=re.findall(pattern,x_str,overlapped=True)
+      # print(matches)
+      RDCOUNT+=len(matches)
+
+print("RD count:",RDCOUNT)
+
+
+sum=HCOUNT+VCOUNT+LDCOUNT+RDCOUNT
+print(sum)
+
+
+print("===========================================================")
+end=time.time()
+print("Time taken: "+str(end-start)+" seconds")
